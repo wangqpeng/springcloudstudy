f4b92bbd21bcfa23bf4b839290000ae2e7502b0a
wangqipeng <944634179@qq.com>
2019-06-30 23:31:42 +0800

    springcloud class:no.1

---
+++ spring_cloud_api/src/main/java/com/spring/cloud/entity/Dept.java
@@ -0,0 +1,28 @@
+package com.spring.cloud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@Data
+public class Dept implements Serializable {
+    /**
+     *  部门编码 主键
+     */
+    private Long deptNo;
+    /**
+     * 部门名称
+     */
+    private String deptName;
+    /**
+     * 数据库名称
+     */
+    private String dbSource;
+
+}
